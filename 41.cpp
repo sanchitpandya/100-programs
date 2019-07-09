5130b8729c7379f0e50f734ae46d2e63e85a55a8
Sanchit Pandya <43060173+sanchitpandya@users.noreply.github.com>
2019-07-08 20:49:16 -0700

    Add files via upload

---
+++ 41.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[100],s2[100];
+    printf("Enter first string: ");
+    gets(s1);
+    printf("Enter second string: ");
+    gets(s2);
+    int flag=0;
+    if(strlen(s1)==strlen(s2))
+    {
+        for(int i=0;i<strlen(s1);i++)
+        {
+           if(s1[i]!=s2[i])
+           {
+              flag=1;
+              break;
+           }
+        }
+        if(flag==0)
+          printf("Both the strings are identical.");
+        else
+          printf("Both the strings are not identical.");           
+    }
+    else
+        printf("Both strings can't be identical since lengths of both the strings are different");
+    
+    int x;
+    scanf("%d",&x);
+}
